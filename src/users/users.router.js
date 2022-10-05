627e04d10d6ebf9fd197806ca7219e955c38c4a3
carlosdb01 <carlos.andres.davila03@gmail.com>
2022-10-05 18:19:43 -0500

    First commit

---
+++ src/users/users.router.js
@@ -0,0 +1,15 @@
+//* /users [ POST, GET]
+//* /users:id [GET]
+
+const router = require('express').Router()
+
+const usersServices = require('./users.services')
+
+router.get('/users', usersServices.getUsers)
+
+router.post('/users', usersServices.createNewUsers)
+
+router.get('/users/:id', usersServices.getOneUsers)
+
+
+module.exports = router
